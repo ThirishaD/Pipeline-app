494d26f6200d60ab5d7003bd0cdb073a0cf9e39f
Thrisha Babu <thirishudoll511@gmail.com>
2025-02-24 14:34:14 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 8080;
+
+// Basic Route
+app.get('/', (req, res) => {
+    res.send('This is Stanley');
+});
+
+// Start Server
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+});
+
+
+// Health Check Route
+app.get('/health', (req, res) => {
+    res.status(200).send('OK');
+});
